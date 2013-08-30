80246d3dfdd6a2c3ff0a2b35fe454a4c160115ba
Yang Hu <hueyhy@gmail.com>
2013-08-30 11:27:19 -0400

    harmonics.py tweaked

--- harmonics.py
+++ harmonics.py
@@ -6,7 +6,7 @@ def harmonic_series(fbase,len=None):
 	if len == None:
 		len=0.03
 	x=numpy.arange(0,len,1/44000)
-	harmo_f = [fbase*n for n in range(10)]
+	harmo_f = [fbase*n for n in range(1,10)]
 	t=[]
 	for freq in harmo_f:
 		t.append(sin(2*pi*freq*x))
@@ -27,14 +27,9 @@ def just_into_tune():
 		return notes[ind]
 
 	tune={}
-
 	key='C'
-
 	tune['C']=100
-
-
 	newkey = getnext(key)
-
 	while newkey!='C':
 		tmp_freq = (tune[key]*1.5)
 		while tmp_freq >= 200:
@@ -42,7 +37,6 @@ def just_into_tune():
 		tune[newkey] = tmp_freq
 		key = newkey
 		newkey = getnext(key)
-
 	C4 = 261.63
 	freqs = [tune[key]*C4/100 for key in notes]
 	
@@ -58,19 +52,18 @@ def well_tempered():
 tunes = well_tempered()
 
 Gs_series = harmonic_series(tunes['g'],0.5)
-A_series = harmonic_series(tunes['A'],0.1)
-
-E_series = harmonic_series(tunes['E'],0.1)
-Gs3=Gs_series[1]
-A3=A_series[1]
-A4=A_series[2]
-E5=E_series[2]
+A_series = harmonic_series(tunes['A'],5)
+E_series = harmonic_series(tunes['E'],5)
+Gs4=Gs_series[0]
+A4=A_series[0]
+A5=A_series[1]
+E5=E_series[1]
 fifth = A4+E5
 #semi = Gs3+A3
 
 x=numpy.arange(0,0.1,1/44000)
 
-
 plot(fifth)
-plot(A3)
+plot(A4)
+plot(E5)
 plot(x,semi)
